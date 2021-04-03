e85ddfd697aa0ba5bbe2c387bb95ac58c7be799e
Stelmach Ro <stelmach.ro@gmail.com>
2021-04-03 18:13:15 +0100

    Acquire-release operations

--- aquiare_release.cpp
+++ aquiare_release.cpp
@@ -4,7 +4,7 @@
 #include <mutex>
 #include <cassert>
 
-void aquire_release_fun() {
+void aquire_release_assert_can_fire() {
 
     std::atomic<bool> x { false };
     std::atomic<bool> y { false };
@@ -12,7 +12,7 @@ void aquire_release_fun() {
 
     // Nothing can move after release operation
     //
-    // Nothing can move before aquire operation
+    // Nothing can move before acquire operation
 
     std::thread write_x([&]() {
         x.store(true, std::memory_order_release);
@@ -24,9 +24,9 @@ void aquire_release_fun() {
 
     std::thread read_x_then_y([&]() {
         // x and y are written by different threads, and read by different threads 
-        // the ordering from the relese to the aquire in each case, has no effect
-        // in the opperations in the other threads
-        // e.g the relese operation in the write_x thread, has no ordering effect
+        // the ordering from the release to the acquire in each case, has no effect
+        // in the operations in the other threads
+        // e.g the release operation in the write_x thread, has no ordering effect
         // with the acquire operation in the read_x_then_y thread
         while (!x.load(std::memory_order_acquire)); // while x is false, then spin
         if (y.load(std::memory_order_acquire)) // if y is true then increment the z
@@ -45,15 +45,41 @@ void aquire_release_fun() {
     read_y_then_x.join();
 
  
+    // Assert here still can fire, because x, y store are happening from different
+    // threads than the x, y loads
     assert(z != 0);
-    
-    std::cout << "Z is: " << z << std::endl;
+}
+
+void aquire_release_assert_cannot_fire() {
+    std::atomic<bool> x { false };
+    std::atomic<bool> y { false };
+    std::atomic<int> z { 0 };
+
+    std::thread write_x_and_y([&]() {
+        // relaxed here doesn't matter at all, because nothing can move after release
+        // operation
+        x.store(true, std::memory_order_relaxed);
+        y.store(true, std::memory_order_release);
+    });
+
+    std::thread read_y_then_x([&]() {
+        // Nothing can move before acquire operation
+        while (!y.load(std::memory_order_acquire));
+        if (x.load(std::memory_order_relaxed))
+            ++z;
+    });
+
+    write_x_and_y.join();
+    read_y_then_x.join();
+
+    assert(z != 0);
 
 }
 
 int main() {
 
-    aquire_release_fun();
+    // aquire_release_assert_can_fire();
+    aquire_release_assert_cannot_fire();
 
     return 0;
 }
